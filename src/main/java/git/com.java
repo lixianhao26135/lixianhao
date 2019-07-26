9ea4db394de748ff941328549cb06ca86c636e8a
tom_glb <tom_glb@atguigu.com>
2019-07-27 06:01:10 +0800

    commit git

---
+++ src/main/java/git/com.java
@@ -0,0 +1,8 @@
+package git;
+
+public class com {
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("lol");
+	}
+}
---
+++ src/test/java/git/Test.java
@@ -0,0 +1,8 @@
+package git;
+
+public class Test {
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("lslls");
+	}
+}
